a4895679981db55d6efb95bf385eb4db51d301cc
John Morse <morse81657@gmail.com>
2019-08-26 22:36:09 -0600

    Added logging examples back in

--- View/App.xaml.cs
+++ View/App.xaml.cs
@@ -9,12 +9,17 @@
     /// </summary>
     public partial class App : Application
     {
+        //set the logger
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         static App()
         {
             //configure logging (only set once)
             Logging.ConfigureLog4Net();
 
             DispatcherHelper.Initialize();
+
+            log.Info("Application Initialized");
         }
     }
 }
--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -8,12 +8,17 @@
     /// </summary>
     public partial class MainWindow : Window, IClosable
     {
+        //set the logger
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public MainWindow()
         {
             InitializeComponent();
             Closing += (s, e) => 
             {
                 ViewModelLocator.Cleanup();
+
+                log.Info("Application Closing");
             };
         }
     }
